8b090b6f1bc1cdac62b0fe637a6464789d1f7e45
adrianbruegger <63962845+adrianbruegger@users.noreply.github.com>
2022-05-18 15:08:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello DevOps Course on Heroku:-)');
+res.send('Hello DevOps Course on Heroku! :-)');
 });
 app.listen(PORT, HOST);
 console.log(`Running on
